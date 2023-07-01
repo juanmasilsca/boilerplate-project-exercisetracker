cf161c8a7138ba57ad55ce4042a311ba03cce5fb
Juan Manuel Silva <74413106+juanmasilsca@users.noreply.github.com>
2023-06-30 23:58:28 -0300

    prueba

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 const formatDate = (date) => {
-  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+  return `${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()}`;
 }
 
 app.post('/api/users', async (req, res) => {
@@ -50,11 +50,11 @@ app.get('/api/users', async (req, res) => {
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
   const id = req.params._id;
+  const date = req.body.date ? new Date(req.body.date) : new Date();
   const ejercicio = new Ejercicio({
-    //userId: id,
     description: req.body.description,
     duration: req.body.duration,
-    date: new Date(req.body.date).toDateString() || new Date().toDateString()
+    date: formatDate(date)
   })
   try {
     const userToSave = await Usuario.findByIdAndUpdate(
@@ -65,7 +65,7 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
       username: userToSave.username,
       description: ejercicio.description,
       duration: ejercicio.duration,
-      date: ejercicio.date,
+      date: date.toDateString(),
       _id: userToSave._id
     });
     // const ejeToSave = await ejercicio.save();
@@ -78,15 +78,19 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 app.get('/api/users/:_id/logs', async (req, res) => {
   let id = req.params._id;
   const limit = req.query.limit? Number(req.query.limit) : 100;
-  const from = req.query.from? req.query.from : formatDate(new Date(-8640000000000000));
-  const to = req.query.to? req.query.to : formatDate(new Date(8640000000000000));
+  const from = req.query.from ? req.query.from: formatDate(new Date(-8640000000000000));
+  const to = req.query.to ? req.query.to : formatDate(new Date(8640000000000000));
+  //const from = req.query.from ? req.query.from: new Date(-8640000000000000).toISOString();
+  //const to = req.query.to ? req.query.to : new Date(8640000000000000).toISOString();
+  console.log(from, to);
   try {
     let logs = await Usuario.aggregate([
       { $match : { _id : new mongoose.Types.ObjectId(id) } },
       { $project: {
         username: 1,
         count: { $size: "$log" },
-        _id: 1, 
+        _id: 1,
+        //log: 1
         log: {
           $filter: {
             input: "$log",
@@ -107,7 +111,17 @@ app.get('/api/users/:_id/logs', async (req, res) => {
       }},
       { $unset: "log._id"}
     ]);
-    logs[0].log.f
+    let updatedLogs = logs[0].log.map(e => {
+      const newLog = {};
+      newLog['description'] = e.description;
+      newLog['duration'] = e.duration;
+      newLog['date'] = new Date(e.date).toDateString();
+      newLog['_id'] = e._id;
+
+      return newLog;
+    });
+    logs[0].log = updatedLogs;
+    console.log(logs[0]);
     res.send(logs[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
